f7d091b2a90d9ddc9c4c082adc40c2bba4e19aa2
nvquyet <nvquyet23052001@gmail.com>
2022-03-09 22:54:55 +0700

    update

--- src/page/student/index.js
+++ src/page/student/index.js
@@ -25,10 +25,10 @@ function Student() {
         <button>Delete</button>
       </tr>
       <tr>
-        <td>Nguyễn văn Quyết</td>
+        <td>Nguyễn Văn Linh</td>
         <td>21</td>
-        <td>0232132213</td>
-        <td>nvquyet23052001@gmail.com</td>
+        <td>0829219954</td>
+        <td>linhvip@gmail.com</td>
         <td>
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
@@ -38,10 +38,10 @@ function Student() {
         <button>Delete</button>
       </tr>
       <tr>
-        <td>Nguyễn văn Quyết</td>
+        <td>Nguyễn văn Tuấn</td>
         <td>21</td>
-        <td>0232132213</td>
-        <td>nvquyet23052001@gmail.com</td>
+        <td>078215214</td>
+        <td>tuannvph11123@gmail.com</td>
         <td>
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
