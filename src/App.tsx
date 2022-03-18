6cd1d09cdcfd266897004c51aeb527e1aaa84b53
nvquyet <nvquyet23052001@gmail.com>
2022-03-18 16:35:09 +0700

    props

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./page/login";
 import Student from "./page/student";
+
+type studentType = [
+  {
+    name: string;
+    age: number;
+    address: string;
+  }
+];
+
+const arrStudent = [
+  {
+    name: "quyet",
+    age: 20,
+    address: "abc",
+  },
+];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
-        <Student />
+        <Student rows={arrStudent} />
       </header>
     </div>
   );
---
+++ src/page/student/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./student.module.css";
+
+type row = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+type TableProps = {
+  rows: row[];
+};
+
+function Student({ rows }: TableProps) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Tên</th>
+          <th>Tuổi</th>
+          <th>Address</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, i) => (
+          <tr key={i}>
+            <td>{row.name}</td>
+            <td>{row.age}</td>
+            <td>{row.address}</td>
+            <td>
+              <button>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Student;
