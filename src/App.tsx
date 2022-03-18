7f9a040c2a05839a980e8f67827b4703aba1458c
nvquyet <nvquyet23052001@gmail.com>
2022-03-18 18:10:41 +0700

    feat: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./page/login";
 import Student from "./page/student";
 
-type studentType = [
-  {
-    name: string;
-    age: number;
-    address: string;
-  }
-];
-
-const arrStudent = [
-  {
-    name: "quyet",
-    age: 20,
-    address: "abc",
-  },
-];
+//adasdasd
+type studentType = {
+  name: string;
+  age: string;
+  address: string;
+};
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [isDisplay, setIsDisplay] = useState<boolean>(true);
+
+  const [name, setName] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+
+  const [student, setStudent] = useState<studentType[]>([
+    {
+      name: "quyet",
+      age: "20",
+      address: "abc",
+    },
+  ]);
+
+  const getDataForm = () => {
+    const data: any = {
+      name: name,
+      address: address,
+      age: age,
+    };
+
+    setStudent([...student, data]);
+    console.log(student);
+  };
+
   return (
     <div className="App">
+      <button onClick={() => setCount(count + 1)}>Increase count</button>
+      <button onClick={() => setIsDisplay(!isDisplay)}>Display</button>
+      <p>{count}</p>
       <header className="App-header">
-        <Student rows={arrStudent} />
+        {isDisplay && <Student rows={student} />}
       </header>
+      <form>
+        <div className="form__box">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+          />
+        </div>
+        <div className="form__box">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            name="address"
+            onChange={(e) => setAddress(e.target.value)}
+            id="address"
+          />
+        </div>
+        <div className="form__box">
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            name="age"
+            id="age"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            getDataForm();
+          }}
+        >
+          Add
+        </button>
+      </form>
     </div>
   );
 }
--- src/page/student/index.tsx
+++ src/page/student/index.tsx
@@ -3,7 +3,7 @@ import "./student.module.css";
 
 type row = {
   name: string;
-  age: number;
+  age: string;
   address: string;
 };
 
@@ -13,28 +13,30 @@ type TableProps = {
 
 function Student({ rows }: TableProps) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Tên</th>
-          <th>Tuổi</th>
-          <th>Address</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, i) => (
-          <tr key={i}>
-            <td>{row.name}</td>
-            <td>{row.age}</td>
-            <td>{row.address}</td>
-            <td>
-              <button>Delete</button>
-            </td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Tên</th>
+            <th>Tuổi</th>
+            <th>Address</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {rows.map((row, i) => (
+            <tr key={i}>
+              <td>{row.name}</td>
+              <td>{row.age}</td>
+              <td>{row.address}</td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
